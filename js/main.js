be07699e42774d2151546eeda0d17bde32f6b245
tarekalqaddy <tarek.alqaddy@gmail.com>
2017-11-17 22:13:12 +0200

    Add home, projects and services layouts.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// $('#fullpage').fullpage({
+//     slidesNavigation: true
+// });
+//
+// function move() {
+//     $.fn.fullpage.moveSlideLeft()
+// }
+
+$('#fullpage').onepage_scroll({
+    animationTime: 600
+})
